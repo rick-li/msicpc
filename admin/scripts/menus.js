8b9eec98bd88d51643013192a187fa3535911257
Rick Li <racke1983cn@gmail.com>
2014-08-02 21:59:49 +0800

    complete menus admin

--- admin/scripts/menus.js
+++ admin/scripts/menus.js
@@ -1,19 +1,46 @@
 app.controller('MenusCtrl', function($scope, $log, $http, $sfileupload) {
-  $scope.selectedItem = {
-    categories: []
+  $scope.new = function() {
+    $scope.selectedItem = {
+      categories: []
+    };
   };
 
-  $scope.categories = [];
+  $scope.new();
 
-  $.get('/admin/menus').done(function(menus) {
-    $scope.menus = menus;
-    $scope.$apply();
-  });
+  $scope.init = function() {
+    if(!$scope.preSelectedIdx){
+      $scope.preSelectedIdx = 0;
+    }
+    $scope.categories = [];
 
-  $.get('/admin/cates').done(function(cates) {
-    $scope.categories = cates;
-    $scope.$apply();
-  });
+    $.get('/admin/menus').done(function(menus) {
+      $scope.menus = menus;
+      //replace group to reference
+      menus.forEach(function(menu) {
+        (menu.group && menu.group._id) && (menu.group = _.findWhere(menus, {
+          _id: menu.group._id
+        }));
+
+      });
+
+      $scope.selectedItem = menus[$scope.preSelectedIdx];
+
+      $scope.$apply();
+    });
+
+    $.get('/admin/cates').done(function(cates) {
+      $scope.categories = cates;
+
+      $scope.$apply();
+    });
+  };
+
+  $scope.init();
+
+
+  $scope.cateNames = function(menu) {
+    return _.pluck(menu.categories, 'name');
+  };
 
   $scope.removeCategory = function(idx, cate) {
 
@@ -28,23 +55,68 @@ app.controller('MenusCtrl', function($scope, $log, $http, $sfileupload) {
 
 
   $scope.fileChanged = function(finput) {
-    
+
     var fileReader = new FileReader();
     fileReader.onloadend = function(e) {
       $scope.selectedItem.image = e.target.result;
       $scope.$apply();
-    }
-    
+    };
+
     fileReader.readAsDataURL(finput.files[0]);
   };
 
-  $scope.submit = function(item) {
-    $.post('/admin/menus', item).done(function() {
-      console.log('admin menu posted done.');
+  $scope.moveup = function(item) {
+    var idx = $scope.menus.indexOf(item);
+    if (idx === 0) {
+      return;
+    }
+    var targetIdx = idx - 1;
+    $scope.doMove(item, idx, targetIdx);
+  };
+
+  $scope.movedown = function(item) {
+    var idx = $scope.menus.indexOf(item);
+    if (idx === $scope.menus.length - 1) {
+      return;
+    }
+    var targetIdx = idx + 1;
+    $scope.doMove(item, idx, targetIdx);
+  };
+
+  $scope.doMove = function(item, idx, targetIdx) {
+    var targetItem = $scope.menus[targetIdx];
+    //swap pos
+    $scope.menus[idx] = targetItem;
+    $scope.menus[targetIdx] = item;
+    //swap idx
+    var tmpOrder = item.order;
+    item.order = targetItem.order;
+    targetItem.order = tmpOrder;
+  };
+
+  $scope.delete = function(item) {
+    $.ajax({method: 'delete', url: '/admin/menus', data: {id: item._id}}).done(function(res) {
+      if (res.status === 'error') {
+        alert(JSON.stringify(res));
+      } else {
+        $scope.init();
+        $scope.$apply();
+      }
     });
   };
 
-  $scope.debug = function() {
-    console.log($scope.selectedParentMenu);
-  }
+  $scope.submit = function(item) {
+    $scope.preSelectedIdx = $scope.menus.indexOf(item);
+    if($scope.preSelectedIdx === -1){
+      $scope.preSelectedIdx = 0;
+    }
+    $.post('/admin/menus', item).done(function(res) {
+      if (res.status === 'error') {
+        alert(JSON.stringify(res));
+      } else {
+        $scope.init();
+        $scope.$apply();
+      }
+    });
+  };
 });
